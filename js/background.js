1237c279973174a337919fe5cdb755f98c052c2f
“lcc” <chengcheng.li@51hgp.com>
2023-10-10 16:51:52 +0800

    feat: copyOver func

--- js/background.js
+++ js/background.js
@@ -18,49 +18,78 @@ const bgCmdHandler = {
 
 Notifications.onButtonClicked.addListener((notificationId, btnIndex) => {
   // console.log('btn clicked', notificationId, btnIndex);
-  if(notificationId === 'locationNotify'){
+  if(notificationId === locationNotifyHandler.notificationId){
+    locationNotifyHandler.onBtnClick(btnIndex)
+  }
+})
+
+const locationNotifyHandler = {
+  notificationId: 'locationNotifyId',
+  locationInfo: [], // 当前iframe信息
+  showBtns: true,
+  notifyOptions: {
+    iconUrl: '../assets/img/logo.png',
+    type: 'list',
+    title: '',
+    message: '',
+    // buttons: [{title: '复制hash'}, {title: '复制全部'}],
+    get buttons(){
+      return locationNotifyHandler.showBtns ? [{title: '复制hash'}, {title: '复制全部'}] : undefined
+    },
+    get items(){
+      return this.type === 'list' ? locationNotifyHandler.locationInfo : undefined
+    }
+  },
+  locationNotify(items){
+    // 发送chrome提示：location
+    this.notifyOptions.type = 'list'
+    this.notifyOptions.title = '获取路径'
+    this.notifyOptions.message = ''
+    this.showBtns = true
+    this.locationInfo = items
+    Notifications.create(this.notificationId, this.notifyOptions ,(id) => {
+      console.log(id);
+    })
+    return this.notificationId
+  },
+  copyOver(title = '', msg = ''){
+    this.notifyOptions.type = 'basic'
+    this.notifyOptions.title = title
+    this.notifyOptions.message = msg
+    this.showBtns = false
+    // 不用修改items，因为basic类型不会显示items
+    Notifications.create(this.notificationId, this.notifyOptions, (id) => {
+      console.log(id);
+    })
+  },
+  findItems(key){
+    if(key === 'hash')return this.locationInfo.find((i) => i.title === 'hash')
+    if(key === 'href')return this.locationInfo.find((i) => i.title === 'href')
+  },
+  onBtnClick(btnIndex){
     switch (btnIndex) {
       case 0: // 复制hash
-        const hashItems = locationNotifyHandler.findItems('hash')
+        const hashItems = this.findItems('hash')
+        console.log(hashItems);
         if(hashItems){
           copyText(hashItems.message)
+          this.copyOver('复制成功')
+        }else{
+          this.copyOver('复制失败', 'hash不存在')
         }
         break;
-      case 1: // 复制全部
-        const hrefItems = locationNotifyHandler.findItems('href')
+        case 1: // 复制全部
+        const hrefItems = this.findItems('href')
         if(hrefItems){
           copyText(hrefItems.message)
+          this.copyOver('复制成功')
+        }else{
+          this.copyOver('复制失败', '路径不存在')
         }
         break;
     
       default:
         break;
     }
-  }
-})
-
-const locationNotifyHandler = {
-  locationInfo: [], // 当前iframe信息
-  // 发送chrome提示：location
-  locationNotify(items){
-    const id = 'locationNotify'
-    this.locationInfo = items
-    Notifications.create(id, {
-      iconUrl: '../assets/img/logo.png',
-      type: 'list',
-      title: '解析结果',
-      message: '',
-      buttons: [{title: '复制hash'}, {title: '复制全部'}],
-      items
-    },(id) => {
-      console.log(id);
-    })
-    return id
-  },
-
-  findItems(key){
-    if(key === 'hash')return this.locationInfo.find((i) => i.title === 'hash')
-    if(key === 'href')return this.locationInfo.find((i) => i.title === 'href')
-    
   }
 }
--- js/content.js
+++ js/content.js
@@ -1,13 +1,13 @@
 if(isTop){
-  const btn = document.createElement('button')
-  const text = document.createTextNode('test')
-  btn.appendChild(text)
-  document.body.prepend(btn)
-  btn.addEventListener('click', ()=>{
-    sendMsg.toBackground('content test').then((res)=>{
-      console.log(res);
-    })
-  })
+  // const btn = document.createElement('button')
+  // const text = document.createTextNode('test')
+  // btn.appendChild(text)
+  // document.body.prepend(btn)
+  // btn.addEventListener('click', ()=>{
+  //   sendMsg.toBackground('content test').then((res)=>{
+  //     console.log(res);
+  //   })
+  // })
 }
 
 const cmdHandler = {
